9b8ffcdb5f451a1935da92f7f61a17903a988b85
Denis V Lebedev <dv.lebedev777@gmail.com>
2015-02-19 10:57:25 +0300

    Update Program.cs

--- QuikConnector.Examples/Program.cs
+++ QuikConnector.Examples/Program.cs
@@ -41,7 +41,7 @@ namespace QuikConnector.Examples
 
                 /*          *****        async transaction        ******
                 *
-                *       15 checks, for 100 milliseconds
+                *       15 checks for 100 milliseconds
                 *       then -> if the order still not executed - killorder
                 */
 
